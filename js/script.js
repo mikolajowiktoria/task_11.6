a441a7e3c6429b5c99c046ae583267e33951fd46
Wiktoria Mikołajów <mikolajow.w@gmail.com>
2018-01-22 15:15:46 +0100

    change style.css

--- js/script.js
+++ js/script.js
@@ -7,44 +7,44 @@ $(function() {
 		}
 		return str;
 	}
-	// Klasa-Obiekt-Prototyp 'Column' z parametrem (name)
+
 	function Column(name) {
 		var self = this;
-		this.id = randomString(); // Metoda randomString() - wyzej
+		this.id = randomString(); 
 		this.name = name;
-		this.$element = createColumn(); // Metoda createColumn() - nizej
-		function createColumn() { // Embedded method
-			// CREATING COMPONENTS OF COLUMNS
-			var $column = $('<div>').addClass('column');
+		this.$element = createColumn(); 
+		function createColumn() { 
+			var $column = $('<div>').addClass('column container');
 			var $columnTitle = $('<h2>').addClass('column-title').text(self.name);
-			var $columnCardList = $('<ul>').addClass('column-card-list');
+			var $columnCardList = $('<ul>').addClass('column-card-list col-lg-12');
 			var $columnDelete = $('<button>').addClass('btn btn-danger btn-delete').text('x');
 			var $columnAddCard = $('<button>').addClass('btn btn-primary add-card').text('Add a card');
-			// ADDING EVENTS
+			
 			$columnDelete.click(function() {
 				self.removeColumn();
 			});
 			$columnAddCard.click(function(event) {
 				self.addCard(new Card(prompt("Enter the name of the card")));
 			});
-			// CONSTRUCTION OF THE COLUMN ELEMENT AND RETURNING THE COLUMN
+			
 			$column.append($columnTitle).append($columnDelete).append($columnAddCard).append($columnCardList);
-			// RETURN OF CREATED COLUMN
+	
 			return $column;
 		}
 	}
     
-    // Implementacja metod: addCard() i removeColumn() do klasy 'Column'
+    
 	Column.prototype = {
-		addCard: function(card) {         // parametrem w () jest karta którą dodajemy
+		addCard: function(card) {        
+            
 			this.$element.children('ul').append(card.$element);
-		},                                // podpinanie card do listy 'ul' w div.column
-		removeColumn: function() {        // w momencie wciśnięcia buttona btn-delete
-			this.$element.remove();       // usuń div.column poprzez remove();
+		},                                
+		removeColumn: function() {       
+			this.$element.remove();       
 		}
 	};
     
-	// Klasa-Obiekt-Prototyp 'Card' z parametrem (description)
+	
 	function Card(description) {
 		var self = this;
 		this.id = randomString();
@@ -52,27 +52,26 @@ $(function() {
 		this.$element = createCard();
 
 		function createCard() {
-			// CREATING THE BLOCKS
+			
 			var $card = $('<li>').addClass('card');
 			var $cardDescription = $('<p>').addClass('card-description').text(self.description);
 			var $cardDelete = $('<button>').addClass('btn btn-danger btn-delete').text('x');
-			// BINDING TO CLICK EVENT
+			
 			$cardDelete.click(function() {
 				self.removeCard();
 			});
-			// COMBINING BLOCKS AND RETURNING THE CARD
+		
 			$card.append($cardDelete).append($cardDescription);
 			return $card;
 		}
 	}
-    // Implementacja metody removeCard() do klasy 'Card'
+  
     Card.prototype = {
         removeCard: function() {
-            this.$element.remove();        // po wcisnieciu buttona usun card
+            this.$element.remove();        
         }
     }
     
-    // Tworzenie obiektu 'board'. addColumn -> Metoda przypięcia column element do board element.
     var board = {
         name: 'Kanban Board',
         addColumn: function(column) {
@@ -82,7 +81,7 @@ $(function() {
         $element: $('#board .column-container')
     };
     
-    // Biblioteka JQuery-UI - drag&drop
+
     function initSortable() {
         $('.column-card-list').sortable({
         connectWith: '.column-card-list',
@@ -90,19 +89,17 @@ $(function() {
         }).disableSelection();
     }
     
-    // po klikneciu na button 'Add a column' prośba o podanie nazwy kolumny i stworzenie nowego obiektu klasy Column z podaną nazwą kolumny. Do obiektu 'board' dodana zostanie nowa kolumna.
+
     $('.create-column').click(function() {
        var name = prompt('Please enter a column name');
        var column = new Column(name);
           board.addColumn(column);    
     });
     
-    // Tworzenie nowym obiektów-kolumn w klasie 'Column'
     var todoColumn = new Column('To do');
     var doingColumn = new Column('Doing');
     var doneColumn = new Column('Done');
     
-    // Dodawanie kolumn do obiektu 'board'
     board.addColumn(todoColumn);
     board.addColumn(doingColumn);
     board.addColumn(doneColumn);
